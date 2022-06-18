ca3d6eaff656ec09cf988da98b4274c2d60cd323
Ph03n1x-5 <79769620+Ph03n1x-5@users.noreply.github.com>
2022-06-18 13:34:24 -0400

    Add files via upload

---
+++ W_Temperature_Functions.py
@@ -0,0 +1,69 @@
+﻿# Import customized functions
+from myCustomFunctions import *
+
+
+def toFixed(value, digits):
+    return "%.*f" % (digits, value)
+
+fileName = ''
+outFile = ''
+
+fileName, outFile = createOutputFile()
+
+# ask user to input file name
+print("Enter the name of the state you are recording the daily temperature for [i.e. Florida, George, New York]")
+
+# decalre variables
+# input statements
+state = input()
+average = 0
+gCount = 0
+i = 0
+
+# ask user how many days will they be recording temperature for
+print("How many days will you record the outdoor temperature for the state of")
+days = checkIntDataType() # call function to check the integer data type and for negative values
+wTemp = [0] * (days)
+gTemp = [0] * (days)
+
+# gather recorded temps into a list
+getData(days, wTemp,state)
+
+# calculate average
+average = calculateAverage(wTemp)
+
+# output statements
+outFile.write("="*60 + "\n")
+outFile.write(("Unsorted Temperature List for the state of " + state).center(60) + "\n")
+outFile.write("="*60 + "\n")   
+writeList(days,wTemp,outFile)
+
+# write minimum, maximum and average temperatures
+outFile.write("="*60 + "\n")
+writeStats(wTemp, average, outFile)
+outFile.write("="*60 + "\n")
+
+# use the built-in sort function to sort the list from lowest to highest
+wTemp.sort()
+
+# output statements
+outFile.write("="*60 + "\n")
+outFile.write(("Sorted Temperature List for the state of " + state).center(60) + "\n")
+outFile.write("="*60 + "\n")
+writeList(days,wTemp,outFile)  
+
+# write the stats to an output file
+writeStats(wTemp, average, outFile)
+outFile.write("="*60 + "\n")
+
+# sort find temps greater than 75
+gCount = findTempGreaterthan75(days, wTemp, gTemp, gCount)
+outFile.write("="*60 + "\n")
+
+# write temps greater than 75 to an output file
+writeTempGreaterthan75(days, gTemp, gCount, outFile)
+outFile.write("="*60 + "\n")
+
+# close external file
+outFile.close()
+# END PROGRAM
---
+++ wTemp_WHILE_LOOP.py
@@ -0,0 +1,88 @@
+﻿# Import customized functions
+from myCustomFunctions import *
+
+
+def toFixed(value, digits):
+    return "%.*f" % (digits, value)
+
+# initialize variables
+fileName = ''
+outFile = ''
+
+fileName,outFile = createOutputFile() # call the function to create the external output file
+print("Enter the name of the state you are recording the daily temperature for [i.e. Florida, George, New York]")
+
+# decalre variables
+# input statements
+state = input()
+i = 0
+print("How many days will you record the outdoor temperature for the state of")
+days = checkIntDataType() # call function to check the integer data type and for negative values
+wTemp = [0] * (days)
+
+# first while loop
+while i < days:
+    print("What is the outdoor temperature in " + state + " on day # " + str(i + 1))
+    wTemp[i] = checkfloatdata()# call function to check float data type and for negative values
+    i = i + 1
+
+# output statements
+outFile.write("="*60 + "\n")
+outFile.write(("Unsorted Temperature List for the state of " + state).center(60) + "\n")
+outFile.write("="*60 + "\n")
+
+# declare more variables
+i = 0
+total = 0
+
+# second while loop
+while i < days:
+    outFile.write(("wTemp[" + str(i + 1) + "] = " + format(wTemp[i], "6.2f") + "°").center(60) + "\n")
+    total = total + wTemp[i]
+    i = i + 1
+    # end while loop    
+
+# calculate average and round 2 decimal places
+average = total / days
+
+# last output statements
+outFile.write("="*60 + "\n")
+# Write minimum, maximum and average temperatures
+outFile.write(("The minimum Temperature = " + format(min(wTemp), "6.2f") + "°").center(60) + "\n")
+outFile.write(("The maximum Temperature = " + format(max(wTemp), "6.2f") + "°").center(60) + "\n")
+outFile.write(("THE AVERAGE DAILY TEMPERATURE = " + format(average, "6.2f") + "°").center(60) + "\n")
+outFile.write("="*60 + "\n")
+outFile.write("="*60 + "\n")
+
+# use the built-in sort function to sort the list from lowest to highest
+wTemp.sort()
+
+# write the sorted list
+# output statements
+outFile.write("="*60 + "\n")
+outFile.write(("Sorted Temperature List for the state of " + state).center(60) + "\n")
+outFile.write("="*60 + "\n")
+
+# declare more variables
+i = 0
+total = 0
+
+# second while loop
+while i < days:
+    outFile.write(("wTemp[" + str(i + 1) + "] = " + format(wTemp[i], "6.2f") + "°").center(60) + "\n")
+    total = total + wTemp[i]
+    i = i + 1
+    # end while loop    
+
+# last output statements
+outFile.write("="*60 + "\n")
+# Write minimum, maximum and average temperatures
+outFile.write(("The minimum Temperature = " + format(min(wTemp), "6.2f") + "°").center(60) + "\n")
+outFile.write(("The maximum Temperature = " + format(max(wTemp), "6.2f") + "°").center(60) + "\n")
+outFile.write(("THE AVERAGE DAILY TEMPERATURE = " + format(average, "6.2f") + "°").center(60) + "\n")
+outFile.write("="*60 + "\n")
+outFile.write("="*60 + "\n")
+
+# close external file
+outFile.close()
+# END PROGRAM
